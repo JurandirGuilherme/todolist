dfbb55720adb56db60059072af261c42fbf576d0
Jurandir Guilherme <jurandirguilherme1@gmail.com>
2024-12-27 12:55:58 -0300

    Coloquei uma toast por vez

--- app/context/ContextProvider.tsx
+++ app/context/ContextProvider.tsx
@@ -9,8 +9,7 @@ export const DataMock = createContext();
 
 function ContextProvider({children}) {
 
-const [messageApi, contextHolder] = message.useMessage();
-
+const [messageApi, contextHolder] = message.useMessage({maxCount:1});
   const [mock, setMock] = useState([]);
   return (
     <DataMock.Provider value={{mock, setMock, messageApi}}>
